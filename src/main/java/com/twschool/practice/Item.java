5a661e1afbce28a63c33d707bb1e3686a7be7b8c
qicaisheng <qicaisheng@qq.com>
2020-07-20 16:34:39 +0800

    should_calculate_tax_given_local_music_CD_item

--- src/main/java/com/twschool/practice/Item.java
+++ src/main/java/com/twschool/practice/Item.java
@@ -1,19 +1,24 @@
 package com.twschool.practice;
 
+import java.math.BigDecimal;
+
 public class Item {
     private final String category;
     private final String from;
     private final int amount;
-    private final double unitPrice;
+    private final BigDecimal unitPrice;
 
-    public Item(String category, String from, int amount, double unitPrice) {
+    public Item(String category, String from, int amount, BigDecimal unitPrice) {
         this.category = category;
         this.from = from;
         this.amount = amount;
         this.unitPrice = unitPrice;
     }
 
-    public int localTax() {
-        return 0;
+    public BigDecimal localTax() {
+        if (category.equals("BOOK")) {
+            return BigDecimal.ZERO.setScale(2, BigDecimal.ROUND_UP);
+        }
+        return unitPrice.multiply(new BigDecimal("0.10")).setScale(2, BigDecimal.ROUND_UP); 
     }
 }
--- src/test/java/com/twschool/practice/ItemTest.java
+++ src/test/java/com/twschool/practice/ItemTest.java
@@ -3,14 +3,25 @@ package com.twschool.practice;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 public class ItemTest {
 
     @Test
     public void should_calculate_tax_given_local_book_item() {
-        Item item = new Item("BOOK", "LOCAL", 1, 12.49);
-        
-        int tax = item.localTax();
+        Item item = new Item("BOOK", "LOCAL", 1, new BigDecimal("12.49"));
 
-        Assert.assertEquals(0, tax);
+        BigDecimal tax = item.localTax();
+
+        Assert.assertEquals(new BigDecimal("0.00"), tax);
+    }
+
+    @Test
+    public void should_calculate_tax_given_local_music_CD_item() {
+        Item item = new Item("MUSIC_CD", "LOCAL", 1, new BigDecimal("14.99"));
+
+        BigDecimal tax = item.localTax();
+
+        Assert.assertEquals(new BigDecimal("1.50"), tax);
     }
 }
