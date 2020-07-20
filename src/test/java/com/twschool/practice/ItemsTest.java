81229b91f4dd6968ca1f006a604a8d52b917a5d3
qicaisheng <qicaisheng@qq.com>
2020-07-20 18:13:26 +0800

    should_calculate_all_items_tax_3

--- src/test/java/com/twschool/practice/ItemsTest.java
+++ src/test/java/com/twschool/practice/ItemsTest.java
@@ -21,6 +21,19 @@ public class ItemsTest {
                 "Total: 29.83", items.describe());
     }
 
+    @Test
+    public void should_calculate_all_items_tax_3() {
+        List<Item> itemList = getItemList3();
+        Items items = new Items(itemList);
+
+        Assert.assertEquals("1 imported bottle of perfume: 32.19\n" +
+                "1 bottle of perfume: 20.89\n" +
+                "1 packet of headache pills: 9.75\n" +
+                "1 imported box of chocolates: 11.82\n" +
+                "Sales Taxes: 6.67\n" +
+                "Total: 74.65", items.describe());
+    }
+
     private List<Item> getItemList1() {
         Item item1 = new Item(ItemCategory.BOOK, ItemFrom.LOCAL, 1, new BigDecimal("12.49"));
         Item item2 = new Item(ItemCategory.MUSIC_CD, ItemFrom.LOCAL, 1, new BigDecimal("14.99"));
@@ -28,5 +41,12 @@ public class ItemsTest {
         return Arrays.asList(item1, item2, item3);
     }
 
+    private List<Item> getItemList3() {
+        Item item1 = new Item(ItemCategory.BOTTLE_OF_PERFUME, ItemFrom.IMPORTED, 1, new BigDecimal("27.99"));
+        Item item2 = new Item(ItemCategory.BOTTLE_OF_PERFUME, ItemFrom.LOCAL, 1, new BigDecimal("18.99"));
+        Item item3 = new Item(ItemCategory.PACKET_OF_HEADACHE_PILLS, ItemFrom.LOCAL, 1, new BigDecimal("9.75"));
+        Item item4 = new Item(ItemCategory.BOX_OF_CHOCOLATE_BAR, ItemFrom.IMPORTED, 1, new BigDecimal("11.25"));
+        return Arrays.asList(item1, item2, item3, item4);
+    }
 
 }
