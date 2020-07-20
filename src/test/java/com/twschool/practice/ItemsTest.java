c6c8bef9caab4f494ae067321311dfbf19b52dc6
qicaisheng <qicaisheng@qq.com>
2020-07-20 18:15:01 +0800

    should_calculate_all_items_tax_2

--- src/test/java/com/twschool/practice/ItemsTest.java
+++ src/test/java/com/twschool/practice/ItemsTest.java
@@ -22,6 +22,17 @@ public class ItemsTest {
     }
 
     @Test
+    public void should_calculate_all_items_tax_2() {
+        List<Item> itemList = getItemList2();
+        Items items = new Items(itemList);
+
+        Assert.assertEquals("1 imported box of chocolates: 10.50\n" +
+                "1 imported bottle of perfume: 54.63\n" +
+                "Sales Taxes: 7.63\n" +
+                "Total: 65.13", items.describe());
+    }
+
+    @Test
     public void should_calculate_all_items_tax_3() {
         List<Item> itemList = getItemList3();
         Items items = new Items(itemList);
@@ -39,6 +50,12 @@ public class ItemsTest {
         Item item2 = new Item(ItemCategory.MUSIC_CD, ItemFrom.LOCAL, 1, new BigDecimal("14.99"));
         Item item3 = new Item(ItemCategory.CHOCOLATE_BAR, ItemFrom.LOCAL, 1, new BigDecimal("0.85"));
         return Arrays.asList(item1, item2, item3);
+    }
+
+    private List<Item> getItemList2() {
+        Item item1 = new Item(ItemCategory.BOX_OF_CHOCOLATE_BAR, ItemFrom.IMPORTED, 1, new BigDecimal("10.00"));
+        Item item2 = new Item(ItemCategory.BOTTLE_OF_PERFUME, ItemFrom.IMPORTED, 1, new BigDecimal("47.50"));
+        return Arrays.asList(item1, item2);
     }
 
     private List<Item> getItemList3() {
