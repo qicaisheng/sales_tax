8f639c1672e82c7cb6a776285f38a2e5201df36c
qicaisheng <qicaisheng@qq.com>
2020-07-20 21:56:21 +0800

    remove unused code

--- src/main/java/com/twschool/practice/Item.java
+++ src/main/java/com/twschool/practice/Item.java
@@ -18,23 +18,7 @@ public class Item {
     public TaxStrategy getTaxStrategy() {
         return taxStrategy;
     }
-
-    public BigDecimal localTax() {
-        BigDecimal localTax = BigDecimal.ZERO;
-        if (!itemValue.getCategory().isBookFoodAndMedicalCategory()) {
-            localTax = itemValue.getUnitPrice().multiply(new BigDecimal("0.10")).multiply(BigDecimal.valueOf(itemValue.getAmount()));
-        }
-        return localTax.setScale(2, BigDecimal.ROUND_UP);
-    }
-
-    public BigDecimal importedTax() {
-        BigDecimal localTax = BigDecimal.ZERO;
-        if (itemValue.getFrom() == ItemFrom.IMPORTED) {
-            localTax = itemValue.getUnitPrice().multiply(new BigDecimal("0.05")).multiply(BigDecimal.valueOf(itemValue.getAmount()));
-        }
-        return localTax.setScale(2, BigDecimal.ROUND_UP);
-    }
-
+    
     public BigDecimal totalPrice() {
         BigDecimal price = itemValue.getUnitPrice().multiply(BigDecimal.valueOf(itemValue.getAmount()));
         return price.add(taxStrategy.tax());
@@ -43,9 +27,5 @@ public class Item {
     public String describe() {
         String itemFrom = itemValue.getFrom() == ItemFrom.IMPORTED ? "imported " : ""; 
         return String.format("%d %s%s: %s", itemValue.getAmount(), itemFrom, itemValue.getCategory().toString(), totalPrice().toString());
-    }
-
-    public BigDecimal tax() {
-        return importedTax().add(localTax());
     }
 }
