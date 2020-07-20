2b7a3b4e14b03bfaa6bf7e062b10f0964818bc77
qicaisheng <qicaisheng@qq.com>
2020-07-20 17:01:13 +0800

    extract isBookFoodAndMedicalCategory method

--- src/main/java/com/twschool/practice/ItemCategory.java
+++ src/main/java/com/twschool/practice/ItemCategory.java
@@ -1,5 +1,15 @@
 package com.twschool.practice;
 
 public enum ItemCategory {
-    BOOK, MUSIC_CD, CHOCOLATE_BAR, BOX_OF_CHOCOLATE_BAR, BOTTLE_OF_PERFUME, PACKET_OF_HEADACHE_PILLS;
+    BOOK("Books"), MUSIC_CD("Audio-Visual"), CHOCOLATE_BAR("Food"), BOX_OF_CHOCOLATE_BAR("Food"), BOTTLE_OF_PERFUME("Makeup"), PACKET_OF_HEADACHE_PILLS("Medical");
+
+    private String bigCategory;
+
+    ItemCategory(String bigCategory) {
+        this.bigCategory = bigCategory;
+    }
+
+    public boolean isBookFoodAndMedicalCategory() {
+        return bigCategory.equals("Books") || bigCategory.equals("Food") || bigCategory.equals("Medical");
+    }
 }
---
+++ src/test/java/com/twschool/practice/ItemCategoryTest.java
@@ -0,0 +1,45 @@
+package com.twschool.practice;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ItemCategoryTest {
+
+    @Test
+    public void should_know_BOOK_category_is_book_fond_and_medical_category() {
+        ItemCategory itemCategory = ItemCategory.BOOK;
+
+        boolean isBookFoodAndMedicalCategory = itemCategory.isBookFoodAndMedicalCategory();
+
+        Assert.assertTrue(isBookFoodAndMedicalCategory);
+    }
+
+    @Test
+    public void should_know_CHOCOLATE_BAR_category_is_book_fond_and_medical_category() {
+        ItemCategory itemCategory = ItemCategory.CHOCOLATE_BAR;
+
+        boolean isBookFoodAndMedicalCategory = itemCategory.isBookFoodAndMedicalCategory();
+
+        Assert.assertTrue(isBookFoodAndMedicalCategory);
+    }
+
+    @Test
+    public void should_know_BOX_OF_CHOCOLATE_BAR_category_is_book_fond_and_medical_category() {
+        ItemCategory itemCategory = ItemCategory.BOX_OF_CHOCOLATE_BAR;
+
+        boolean isBookFoodAndMedicalCategory = itemCategory.isBookFoodAndMedicalCategory();
+
+        Assert.assertTrue(isBookFoodAndMedicalCategory);
+    }
+
+    @Test
+    public void should_know_PACKET_OF_HEADACHE_PILLS_category_is_book_fond_and_medical_category() {
+        ItemCategory itemCategory = ItemCategory.PACKET_OF_HEADACHE_PILLS;
+
+        boolean isBookFoodAndMedicalCategory = itemCategory.isBookFoodAndMedicalCategory();
+
+        Assert.assertTrue(isBookFoodAndMedicalCategory);
+    }
+}
