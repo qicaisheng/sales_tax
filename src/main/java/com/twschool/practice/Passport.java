2e9a52b68db2cf18fe645e8b54db4aa03003b0ca
qicaisheng <qicaisheng@qq.com>
2020-07-20 18:59:16 +0800

    extract constant

--- src/main/java/com/twschool/practice/Passport.java
+++ src/main/java/com/twschool/practice/Passport.java
@@ -1,13 +1,14 @@
 package com.twschool.practice;
 
 public class Passport {
-    private String number;
+    public static final int FOREIGN_PASSPORT_NUMBER_LENGTH = 18;
+    private final String number;
 
     public Passport(String number) {
         this.number = number;
     }
 
     public boolean isForeignPassport() {
-        return number.length() == 18;
+        return number.length() == FOREIGN_PASSPORT_NUMBER_LENGTH;
     }
 }
