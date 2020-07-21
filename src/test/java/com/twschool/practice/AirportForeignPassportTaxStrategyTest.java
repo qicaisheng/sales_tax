576b9af7ae2c43c6823a21c8269233acc346a151
qicaisheng <qicaisheng@qq.com>
2020-07-21 10:36:31 +0800

    refactor test

--- src/test/java/com/twschool/practice/AirportForeignPassportTaxStrategyTest.java
+++ src/test/java/com/twschool/practice/AirportForeignPassportTaxStrategyTest.java
@@ -9,25 +9,28 @@ public class AirportForeignPassportTaxStrategyTest {
 
     @Test
     public void should_calculate_local_tax_given_items() {
-        AirportForeignPassportTaxStrategy airportForeignPassportTaxStrategy = new AirportForeignPassportTaxStrategy(new ItemValue(ItemCategory.BOOK, ItemFrom.IMPORTED, 1, new BigDecimal("12.49")));
+        ItemValue itemValue = new ItemValue(ItemCategory.BOOK, ItemFrom.IMPORTED, 1, new BigDecimal("12.49"));
+        AirportForeignPassportTaxStrategy airportForeignPassportTaxStrategy = new AirportForeignPassportTaxStrategy(itemValue);
         
-        BigDecimal tax = airportForeignPassportTaxStrategy.localTax(airportForeignPassportTaxStrategy.getItemValue());
+        BigDecimal tax = airportForeignPassportTaxStrategy.localTax(itemValue);
 
         Assert.assertEquals(new BigDecimal("1.25"), tax);
     }
     
     @Test
     public void should_calculate_imported_tax_given_items() {
-        AirportForeignPassportTaxStrategy airportForeignPassportTaxStrategy = new AirportForeignPassportTaxStrategy(new ItemValue(ItemCategory.BOOK, ItemFrom.IMPORTED, 1, new BigDecimal("12.49")));
+        ItemValue itemValue = new ItemValue(ItemCategory.BOOK, ItemFrom.IMPORTED, 1, new BigDecimal("12.49"));
+        AirportForeignPassportTaxStrategy airportForeignPassportTaxStrategy = new AirportForeignPassportTaxStrategy(itemValue);
 
-        BigDecimal tax = airportForeignPassportTaxStrategy.importedTax(airportForeignPassportTaxStrategy.getItemValue());
+        BigDecimal tax = airportForeignPassportTaxStrategy.importedTax(itemValue);
 
         Assert.assertEquals(new BigDecimal("0.00"), tax);
     }
 
     @Test
     public void should_calculate_tax_given_items() {
-        AirportForeignPassportTaxStrategy airportForeignPassportTaxStrategy = new AirportForeignPassportTaxStrategy(new ItemValue(ItemCategory.BOOK, ItemFrom.IMPORTED, 1, new BigDecimal("12.49")));
+        ItemValue itemValue = new ItemValue(ItemCategory.BOOK, ItemFrom.IMPORTED, 1, new BigDecimal("12.49"));
+        AirportForeignPassportTaxStrategy airportForeignPassportTaxStrategy = new AirportForeignPassportTaxStrategy(itemValue);
 
         BigDecimal tax = airportForeignPassportTaxStrategy.tax();
 
